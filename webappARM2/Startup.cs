c926f686bf034856b54c0bd790276a2f28101891
Nalamala <VardhanNalamala@ToppanMerrillLLC.com>
2019-08-20 16:29:40 +0530

    adding new comment

--- webappARM2/Startup.cs
+++ webappARM2/Startup.cs
@@ -22,6 +22,7 @@ namespace webappARM2
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // adding new comment
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
